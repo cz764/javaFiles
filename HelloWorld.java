3836acc27059627f364ee4aeda838092ccc70e11
cz764 <xzzhuchen@gmail.com>
2014-04-17 16:15:35 -0400

    Create HelloWorld
    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.print("This is Jane ");
+		System.out.println("Nice to see you!");
+	}
+
+}
